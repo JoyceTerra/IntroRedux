ef0abe12697fcaceece895494a4fc363c3daa7bb
JoyceTerra <joyterra@gmail.com>
2018-08-23 14:49:54 +0200

    Array Reducer Exercise

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 const redux = require('redux')
 
-const  initialState = {
-    firstName: 'Charles',
-    lastName: 'Eamnes',
-    age: 75
-}
+const  initialState = [
+    {
+    name: 'First Dog',
+    isAGoodBoy: true
+    }
+]
 
 
 const reducer = ( state = initialState, action = {} ) => {
     switch (action.type) {
-        case 'SET_FIRST_NAME' :
-        return {
+        case 'ADD_DOG' :
+        return [
             ...state,
-            firstName: action.payload
-        }
-        case 'SET_LAST_NAME':
-        return{
-            ...state,
-            lastName: action.payload
-        }
-        case 'SET_AGE':
-        return{
-            ...state,
-            age:action.payload
-        }
-
+            action.payload
+        ]
+        case 'SET_DOGS':
+        
+        return  action.payload.map((obj) =>({...obj}))
+    
         default:
         return state
     }
@@ -36,8 +30,11 @@ console.log('Initial state of the store: ', store.getState())
 store.subscribe(() => console.log('Next state: ', store.getState()))
 
 const action = {
-    type: 'SET_FIRST_NAME',
-    payload: 'ALICE'
+    type:  'ADD_DOG',
+    payload: {
+        name: 'Second dog',
+        isAGoodBoy: false
+    }
 }
 
 store.dispatch(action)
